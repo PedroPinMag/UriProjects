fa08f3cb1396069f3a3c015ebf712de3c1e3f7ab
PedroPinMag <pedropinheiromagalhaes@gmail.com>
2022-08-13 23:19:46 -0300

    1007 - Difference

---
+++ 1007.py
@@ -0,0 +1,8 @@
+entry_values = []
+
+for i in range(4):
+    entry_values.append(int(input()))
+
+diferenca = entry_values[0] * entry_values[1] - entry_values[2] * entry_values[3]
+
+print(f"DIFERENCA = {diferenca}")

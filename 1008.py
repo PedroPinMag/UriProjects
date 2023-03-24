737ece8e353128fbf1c88a97888a9e3769943800
PedroPinMag <pedropinheiromagalhaes@gmail.com>
2023-03-24 19:29:44 -0300

    Add project 1008

---
+++ 1008.py
@@ -0,0 +1,8 @@
+numero_funcionario = int(input(""))
+horas_trabalhadas = int(input(""))
+valor_hora = float(input(""))
+
+salario = horas_trabalhadas * valor_hora
+
+print("NUMBER =", numero_funcionario)
+print("SALARY = U$ %.2f" % salario)

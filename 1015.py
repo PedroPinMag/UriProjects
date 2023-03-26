dce5f6cf1df170b197cf61a2c7f53bb0db95bb15
PedroPinMag <pedropinheiromagalhaes@gmail.com>
2023-03-25 23:59:57 -0300

    Add project 1015

--- 1015.py
+++ 1015.py
@@ -1,5 +1,12 @@
-distancia = int(input())
+ponto1 = input().split()
+ponto2 = input().split()
 
-tempo = distancia * 2
+x1 = float(ponto1[0])
+y1 = float(ponto1[1])
 
-print('{} minutos'.format(tempo))
+x2 = float(ponto2[0])
+y2 = float(ponto2[1])
+
+distancia = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+print('{:.4f}'.format(distancia))
